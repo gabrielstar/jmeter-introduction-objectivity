a72cf65f9f28427fd84fe1eb13a8cec1bd8da9f4
gstarczewski <gstarczewski@objectivity.co.uk>
2020-01-27 14:32:29 +0100

    fixing bats

--- run_jmeter_parameters.bat
+++ run_jmeter_parameters.bat
@@ -1,2 +1,2 @@
-"C:\Users\gstarcze\dev tools\apache-jmeter-5.0\apache-jmeter-5.0\bin\jmeter.bat" ^
+"C:\Users\gstarczewski\tools\apache-jmeter-5.2.1\bin\jmeter.bat" ^
  -n -Jthreads=5 -t "jmeter-introduction.jmx"
